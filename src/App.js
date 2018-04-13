28b595437d81316a2fcc4b838d4557cf79aafa15
Rafael Mendoza <rfgmendoza@gmail.com>
2018-04-12 19:09:05 -0500

    initial work on App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,43 @@ class AddBill extends Component {
     <div className="add-bill">
       <h1>New Bill</h1>
 
+        <label>
+          Bill Title:
+          <input type="text" name = "bill-name" />
+        </label>
+        <br />
+        <label>
+          Amount:
+          <input type="text" name = "bill-name" />
+        </label>
+        <br />
+        <label>
+          Day Due:
+          <input type="text" name = "bill-name" />
+        </label>
+        <br />
+        <button onClick="saveBill">
+            Add Bill
+        </button>
+      
     </div>
+    );
+  }
+}
+
+class UpcomingBills extends Component{
+  render() {
+    return(
+      <div>
+        <BootstrapTable data={this.props.data}>
+           <TableHeaderColumn dataField='name'>
+           	Name
+           </TableHeaderColumn>
+           <TableHeaderColumn dataField='owed'>
+           	Amount Owed
+           	</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
     );
   }
 }
